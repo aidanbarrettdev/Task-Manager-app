d27ec3e9dca8aa63df5252c8482c478db9855b02
aidan barrett <aidbarrett94@gmail.com>
2023-01-14 15:22:36 +0000

    task routes

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const {
+  createTask,
+  getAllTasks,
+  getTask,
+  deleteTask,
+  updateTask,
+} = require("../controllers/taskController");
+const router = express.Router();
+
+//server prefix for task paths = /api/tasks
+
+//GET all of the tasks
+router.get("/", getAllTasks);
+
+//GET one task
+router.get("/:id", getTask);
+
+// POST + a task
+router.post("/", createTask);
+
+//DELETE task
+router.delete("/:id", deleteTask);
+
+//UPDATE task
+
+router.patch("/:id", updateTask);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const app = express();
-
-app.get("/", (req, res) => {
-  res.json({ mssg: "test" });
+require("dotenv").config();
+const taskRoutes = require("./routes/taskRoutes");
+const mongoose = require("mongoose");
+//middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  //log out my requests
+  next();
 });
 
-app.listen(4000, () => {
-  console.log("listening on 4000");
-});
+app.use(express.json()); //access to request.body
+
+//routes
+app.use("/api/tasks", taskRoutes);
+
+//mongoose-connect to db
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //only listen for request once mongoose is connected to DB
+    app.listen(process.env.PORT, () => {
+      console.log("listening on 4000");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
